28136ca56e27fe018eebe1858268e6531df9d6a0
jonathan simonney <jonathan.simonney@gmail.com>
2018-07-05 14:26:48 +0200

    application fully functional.

--- db/schema.rb
+++ db/schema.rb
@@ -12,16 +12,13 @@
 
 ActiveRecord::Schema.define(version: 20180704140813) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
     t.string "resource_type"
-    t.bigint "resource_id"
+    t.integer "resource_id"
     t.string "author_type"
-    t.bigint "author_id"
+    t.integer "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
@@ -38,8 +35,8 @@ ActiveRecord::Schema.define(version: 20180704140813) do
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.inet "current_sign_in_ip"
-    t.inet "last_sign_in_ip"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
@@ -47,9 +44,9 @@ ActiveRecord::Schema.define(version: 20180704140813) do
   end
 
   create_table "order_products", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "product_id"
-    t.bigint "order_id"
+    t.integer "user_id"
+    t.integer "product_id"
+    t.integer "order_id"
     t.integer "quantity"
     t.decimal "unit_price"
     t.decimal "total_price"
@@ -86,15 +83,12 @@ ActiveRecord::Schema.define(version: 20180704140813) do
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.inet "current_sign_in_ip"
-    t.inet "last_sign_in_ip"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "order_products", "orders"
-  add_foreign_key "order_products", "products"
-  add_foreign_key "order_products", "users"
 end
